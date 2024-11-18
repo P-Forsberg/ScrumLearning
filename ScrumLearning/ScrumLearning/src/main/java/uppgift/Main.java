c892b146fcc6509eb1f338d3aa4b4cd5f891522e
josef <yousef.aho@gmail.com>
2024-11-18 10:16:03 +0100

    Användarnamn funktion

--- ScrumLearning/ScrumLearning/src/main/java/uppgift/Main.java
+++ ScrumLearning/ScrumLearning/src/main/java/uppgift/Main.java
@@ -1,9 +1,11 @@
-package uppgift;
+package main.java.uppgift;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import User.UserManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hej");
+        UserManager userManager = new UserManager();
+        userManager.registerUser();
+        userManager.close();
     }
 }
