56b315a5fc3d4d7ad22345c6f20bf1a7e988522c
ceciliachris <cecilia.christianssons@gmail.com>
2024-11-19 13:18:33 +0100

    Lagt till funktion som gör att spelaren kan göra ett eget quiz samt skapat filer för alla options i startmenyn.

---
+++ ScrumLearning/src/Options/ViewStatisticsOption.java
@@ -0,0 +1,11 @@
+package Options;
+
+import menu.MenuOption;
+
+public class ViewStatisticsOption implements MenuOption {
+    @Override
+    public void execute() {
+        System.out.println("Viewing your game stats: ");
+    }
+
+}
