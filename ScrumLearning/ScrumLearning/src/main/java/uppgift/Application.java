d5d6ec3c116afc02e973e248ad17bf8dd1d67e0c
ceciliachris <cecilia.christianssons@gmail.com>
2024-11-20 15:55:42 +0100

    Flyttat filer samt lagt till interface för att spara till fil

--- ScrumLearning/ScrumLearning/src/main/java/uppgift/Application.java
+++ ScrumLearning/ScrumLearning/src/main/java/uppgift/Application.java
@@ -1,23 +1,8 @@
 package main.java.uppgift;
 
-import menu.Menu;
-import options.PlayerQuizMaker;
-import java.io.IOException;
-
 
 public class Application {
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.displayMenu();
 
-        PlayerQuizMaker quizMaker = new PlayerQuizMaker();
-
-        try {
-            quizMaker.loadQuiz();
-        } catch (IOException e) {
-            System.out.println("No previous quiz found, starting fresh.");
-        }
-
-        quizMaker.createQuiz();
     }
 }
