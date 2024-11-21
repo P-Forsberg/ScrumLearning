f570ae6aa4ff44a20c6520236c402a1764a0028d
anton-jj <jappinen.anton@protnmail.com>
2024-11-21 12:07:13 +0100

    fixade escape karakärer

--- ScrumLearning/ScrumLearning/src/main/java/uppgift/question/QuestionRepo.java
+++ ScrumLearning/ScrumLearning/src/main/java/uppgift/question/QuestionRepo.java
@@ -8,6 +8,7 @@ import java.net.http.HttpResponse;
 import java.util.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringEscapeUtils;
 
 public class QuestionRepo {
         public List<Question> TriviaAPI(String api) {
@@ -28,8 +29,9 @@ public class QuestionRepo {
                 List<Map<String, Object>> res = (List<Map<String, Object>>) responseMap.get("results");
 
                 for (Map<String, Object> triviaQuestion : res) {
-                    String questionText = (String) triviaQuestion.get("question");
-                    String correctAnswer = (String) triviaQuestion.get("correct_answer");
+
+                    String questionText = StringEscapeUtils.unescapeHtml4((String)triviaQuestion.get("question"));
+                    String correctAnswer =  StringEscapeUtils.unescapeHtml4((String)triviaQuestion.get("correct_answer"));
                     String diff = (String) triviaQuestion.get("difficulty");
                     String cat = (String) triviaQuestion.get("category");
                     List<String> incorrectAnswers = (List <String>) triviaQuestion.get("incorrect_answers");
