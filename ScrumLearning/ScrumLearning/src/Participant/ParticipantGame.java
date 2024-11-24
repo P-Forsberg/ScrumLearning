e095b1552e970003eda979f7d3a2d524f17c9186
josef <yousef.aho@gmail.com>
2024-11-24 21:11:07 +0100

    Användarnamn

--- ScrumLearning/ScrumLearning/src/Participant/ParticipantGame.java
+++ ScrumLearning/ScrumLearning/src/Participant/ParticipantGame.java
@@ -37,25 +37,46 @@ public class ParticipantGame {
                 int choice = Integer.parseInt(input);
 
                 switch (choice) {
+                    case 1:
+                        System.out.println("One participant selected. Preparing the game for a solo experience.");
+                        participantManager.setNumberOfParticipants(choice);
+                        break;
+                    case 2:
+                        System.out.println("Two participants selected. Setting up a competitive match!");
+                        participantManager.setNumberOfParticipants(choice);
+                        break;
+                    case 3:
+                        System.out.println("Three participants selected. Let the trio battle begin!");
+                        participantManager.setNumberOfParticipants(choice);
+                        break;
+                    case 4:
+                        System.out.println("Four participants selected. A fun group activity awaits!");
+                        participantManager.setNumberOfParticipants(choice);
+                        break;
+                    case 5:
+                        System.out.println("Five participants selected. The more, the merrier!");
+                        participantManager.setNumberOfParticipants(choice);
+                        break;
+                    case 6:
+                        System.out.println("Six participants selected. Maximum fun is guaranteed!");
+                        participantManager.setNumberOfParticipants(choice);
+                        break;
+                    case 7:
+                        System.out.println("\nRestarting the participant selection...\n");
+                        break;
                     case 0:
                         System.out.println("Thank you for playing. Goodbye!");
                         System.exit(0);
                         break;
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                        participantManager.setNumberOfParticipants(choice);
-                        System.out.println("\nYou chose " + choice + " participant(s).");
-                        return;
-                    case 7:
-                        System.out.println("\nRestarting the participant selection...\n");
-                        break;
                     default:
                         System.out.println("Invalid choice. Please select a valid option.");
                 }
+
+                if (choice >= 1 && choice <= 6) {
+                    System.out.println("\nParticipants set to: " + participantManager.getNumberOfParticipants());
+                    return;
+                }
+
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please enter a number.");
             }
